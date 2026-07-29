1c5749bfcb79b81dc2eb39fd0f709a47c611cb37
Mujahed <mimakhdumi@gmail.com>
2026-07-29 13:16:23 +0530

    Milestone 5 - remove FAISS indexing

--- src/rag/__init__.py
+++ src/rag/__init__.py
@@ -1,7 +1,1 @@
 # Smart-MCQ-Solver.src.rag.__init__.py
-
-# src/rag/__init__.py
-from src.rag.rag_pipeline import RAGPipeline
-from src.rag.faiss_index import FAISSIndex
-
-__all__ = ["RAGPipeline", "FAISSIndex"]
